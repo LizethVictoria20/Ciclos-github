520fcd27aac352f590c39125552259d7ff03d540
LizethVictoria20 <41175343+LizethVictoria20@users.noreply.github.com>
2018-08-20 15:19:39 -0500

    Update multi.js

--- multi.js
+++ multi.js
@@ -1,5 +1,3 @@
-//muestre por pantalla la sucesión de números: 1 -1 2 -2 3 -3 4 -4 5 -5 6 -6 7 -7 8 -8 9 -9 10 -10
-
 function multiplicando(num, factor){
   let acumulador = 0
     for(let i = 0; i < factor; i++){
