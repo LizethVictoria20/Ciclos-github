4da9b30bf3165533fb1cd81f33010c6e3e52b3d7
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2018-08-22 21:18:42 -0500

    Intereses

--- interes.js
+++ interes.js
@@ -1,7 +1,7 @@
 //calcular cuantos años tarda en
 //duplicarse un capital depositado al 5% de interés anual.
 const regla = 72;
-let interés = 15
+let interés = 5
 
 let años = 0
 
@@ -9,3 +9,20 @@ for(let i = 0; i <= interés; i++){
   años = regla / interés
   console.log(años)
 }
+
+//SoluciónII
+const interés = 0.05 //Tasa del 5%
+function añosADuplicar(capital){
+
+  let beneficios = capital
+  let años;
+
+  const dobleCapital = capital * 2
+  for(años = 0; beneficios < dobleCapital; años++){
+    const interesAnual = beneficios * interés
+    beneficios = beneficios + interesAnual
+  }
+  return años
+}
+
+
